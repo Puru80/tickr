fb39bcf1146f755de8878d2ef66855a5385ebcf0
Puru80 <puru.agar99@gmail.com>
2025-09-22 20:35:26 +0530

    Initial commit

---
+++ src/main/java/com/example/tickr/tickr/user/UserInfo.java
@@ -0,0 +1,50 @@
+package com.example.tickr.tickr.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return name != null && Objects.equals(name, userInfo.name);
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+}
---
+++ src/main/java/com/example/tickr/tickr/user/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.example.tickr.tickr.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+
+    Optional<UserInfo> findByEmail(String email);
+}
