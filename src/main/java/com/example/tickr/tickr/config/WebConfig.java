91a2eec9e2e02a63f6882ff55fb8ab771a6d52e1
Puru80 <puru.agar99@gmail.com>
2026-02-10 11:34:58 +0530

    Deploymnet finalized

--- src/main/java/com/example/tickr/tickr/config/WebConfig.java
+++ src/main/java/com/example/tickr/tickr/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.tickr.tickr.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-            .allowedOrigins("http://localhost:8081") // Specify allowed frontend origins
+            .allowedOrigins(frontendUrl) // Specify allowed frontend origins
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
             .allowedHeaders("*") // Allow all headers
             .allowCredentials(true) // Allow credentials (cookies, auth tokens)
